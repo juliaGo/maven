460b0f69c881ef80364c0913d96294f3cb319e05
Julia <julia.goldmann@hs-furtwangen.de>
2015-11-17 15:41:02 +0100

    maven zum lokalen Repository adden

---
+++ src/main/java/de/goldmann/App.java
@@ -0,0 +1,13 @@
+package de.goldmann;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/de/goldmann/Klasse.java
@@ -0,0 +1,10 @@
+package de.goldmann;
+import java.util.*;
+public class Klasse{
+   public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Bitte Zeichenkette eingebe:");
+        String eingabe = sc.next();
+	System.out.println(eingabe.toUpperCase());
+   }
+}
